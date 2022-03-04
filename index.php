df939d4376117a3222c0dab654c5a3ffb316b4b4
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2022-03-04 12:28:15 -0500

    Creation of website
    index.php is the introduction page when you access the website.
    sensorreading.php is the page where the  information about the different sensors will be displayed
    The Images folder contains all images that will be used in the website.

---
+++ index.php
@@ -0,0 +1,19 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Intro page to the Computer project website</title>
+</head>
+
+<body>
+    <!-- Show plant picture on top of page -->
+    <ul>
+        <img src="Images/plant.jpg"/></br>
+    </ul>
+
+    <h1>Automatic Plant Watering and Monitoring System</h1>
+    <p>To see the readings from the sensors connected to the plant, click <a href="sensorreading.php"> here</a>.</br>
+        Enjoy!</br></br>
+        Project created by Louis-Philippe Monette</br>
+        During the Winter 2022 semester at Dawson College, in fulfillment of the final Computer Network project.</p>
+</body>
+</html>
---
+++ sensorreading.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Sensor readings page</title>
+    </head>
+    <body>
+        <h1>Plant Status</h1>
+        <p>Here are the current readings for the Moisture sensor:</p></br>
+
+    </body>
+</html>
